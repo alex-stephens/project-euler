40f599996389b19a90de72d8c635faa5b13f98c4
RaviAlexStephens <alexstephens9@gmail.com>
2018-01-22 23:02:49 +1100

    improved solution to problem 1

--- euler_1.py
+++ euler_1.py
@@ -5,10 +5,28 @@
 
 import numpy as np
 
-sum = 0
+'''
+Very fast solution, works for larger numbers
+'''
 
-for i in range(1,1000):
+result = 0
+n = 10**10
+
+def multiples(n):
+    return n * (n+1) // 2
+
+chainSums = [i*multiples((n-1) // i) for i in [3,5,15]]
+
+result = chainSums[0] + chainSums[1] - chainSums[2]
+print(result)
+
+
+'''
+Brute-force solution
+
+for i in range(1,n):
     if (i % 3 is 0) or (i % 5 is 0):
-        sum += i      
+        result += i      
         
-print(sum)
+print(result)
+'''
