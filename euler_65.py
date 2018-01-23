b8fa18c0c6ea5353b400bb1c5b68666b5ec42f2c
RaviAlexStephens <alexstephens9@gmail.com>
2018-01-23 17:36:34 +1100

    solved problem 65

---
+++ euler_65.py
@@ -0,0 +1,33 @@
+# Project Euler
+# Problem 65
+
+# Convergents of e
+
+def convergent(start, array):
+    if len(array) is 0:
+        return start
+    a,b = array[-1],1
+    for i in range(len(array)-1,-1,-1):
+        if i > 0:
+            new = array[i-1]
+        else:
+            new = start
+        temp = b
+        b = a
+        a = new*b + temp
+        
+    return(a,b)
+    
+def getArray(n):
+    k = 1
+    arr = [1] * n
+    if n is 0:
+        arr = []
+    for i in range(n):
+        if (i+2)%3 is 0:
+            arr[i] = 2*(i//3 + 1)
+    return arr
+
+n = 100
+string = str(convergent(2,getArray(n-1))[0])
+print(sum([int(x) for x in string]))
