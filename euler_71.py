02d12d7de69b040b7efcd9d81e054932d22b3462
RaviAlexStephens <a1ex@optusnet.com.au>
2018-01-24 14:42:02 +1100

    solved problem 71 (75 problems solved)

--- euler_71.py
+++ euler_71.py
@@ -3,14 +3,18 @@
 
 # Ordered fractions
 
-a = 1
-b = 8
-target = 2/5
+(x,y) = (3,7)       # target
+targetNum = x/y
+size = 1000000
+minDiff = 1
 
-for _ in range(9):
-    if a/b > target:
-        b -= 1
-    elif a/b < target :
-        a += 1
+for b in range(2,size+1):
+    if b % y == 0:      # skip exact answers
+        continue
     
-    print(str(a) + ' / ' + str(b))
+    a = (b * x) // y
+    if targetNum - a/b < minDiff and (a,b) != (x,y):
+        minDiff = targetNum - a/b
+        (num,denom) = (a,b)
+        
+print(num)
