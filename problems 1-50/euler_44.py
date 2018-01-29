cbb9e2a572372de6378a2405fd8791e48c61b4c3
RaviAlexStephens <alexstephens9@gmail.com>
2018-01-29 21:22:40 +1100

    solved problem 44 - uses dodgy assumption

--- problems 1-50/euler_44.py
+++ problems 1-50/euler_44.py
@@ -3,31 +3,39 @@
 
 # Pentagonal numbers
 
-def pentagonal(n):
+import sys
+sys.path.append('..')
+from euler import isPerfectSquare
+from math import sqrt
+
+# computes the nth pentagonal number
+def pent(n):
     return (n * (3*n - 1)) // 2
 
 # difference between nth and n+1th pentagonal number
 def pentDiff(n):
     return 1 + 3*n
 
-maxVal = 1000
-pent = [pentagonal(i) for i in range(1,maxVal+5)]
+# checks if a number is pentagonal
+def isPent(n):
+    if not isPerfectSquare(1+24*n):
+        return False
+    num = int(sqrt(1 + 24*n))
+    if (num + 1) % 6 == 0:
+        return True
+    return False
 
-diffMin = None
-for i in range(maxVal):
-    for j in range(i,maxVal):
-        sumVals = pent[i] + pent[j]
-        diffVals = pent[j] - pent[i]
-        if sumVals not in pent:
-            continue
-        elif diffVals not in pent:
-            continue
-        if diffMin == None:
-            diffMin = diffVals
-        else:
-            diffMin = min(diffMin, diffVals)
-            print(i)
-            print(j)
-            
-            
-print(diffMin)
+j,k = 0,0
+done = False
+
+while not done:
+    pk = pent(k)
+    
+    for j in range(1,k):
+        pj= pent(j)
+        if isPent(pk - pj) and isPent(pk + pj):
+            done = True
+            break        
+    k += 1
+
+print(pk - pj)
