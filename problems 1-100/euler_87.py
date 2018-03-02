046e4b8d0ebbb9fda1239a8cea19b5421698ef5f
RaviAlexStephens <alexstephens9@gmail.com>
2018-03-02 23:01:45 +1100

    solved problem 87 - prime power triples

--- problems 1-100/euler_87.py
+++ problems 1-100/euler_87.py
@@ -3,27 +3,28 @@
 
 # Prime power triples
 
+import sys
+sys.path.append('../')
 from euler import listPrimes
 import math
-from itertools import product
+from bisect import bisect_left
 
-cap = 50  * 10**6
+cap = 50 * 10**6
 a_cap = int(cap ** (1/2))
-b_cap = int(cap ** (1/3))
-c_cap = int(cap ** (1/4))
 
 primes = listPrimes(int(math.sqrt(cap)))
 numbers = set()
 
-for comb in product(primes,repeat=3):
-    (a,b,c) = comb
-    if a > a_cap or b > b_cap or c > c_cap:
-        continue
+for a in primes:
     
+    b_cap = bisect_left(primes, (cap -  a**2)**(1/3))
+    for b in primes[:b_cap]:
+        
+        c_cap = bisect_left(primes, (cap -  a**2 - b**3)**(1/4))
+        for c in primes[:c_cap]:
     
-    ans = a**2 + b**3 + c**4
-    if ans <= cap:
-        numbers.add(ans)
+            num = a**2 + b**3 + c**4
+            if num < cap:
+                numbers.add(num)
     
-    #print(comb)
 print(len(numbers))
