88f0da3b8b4bb3bb3353b0132c89e33888f8a3aa
Alex Stephens <alexstephens9@gmail.com>
2021-06-18 15:32:58 +0100

    solved problem 51 - it's been a while...

--- problems 1-100/euler_51.py
+++ problems 1-100/euler_51.py
@@ -5,58 +5,68 @@
 
 import sys
 sys.path.append('../')
-from euler import listPrimes, isPrime
-from itertools import combinations
-import time
+from euler import listPrimes
+from itertools import product
 
+class Pattern(object):
+    def __init__(self, string=''):
+        self.string = string
+        self.numbers = []
+        self.count = 0
 
-def findOccurences(string, c):
-    return [i for i, letter in enumerate(string) if letter == c]
+    def add(self, number):
+        self.numbers.append(number)
+        self.count += 1
 
+    def __repr__(self):
+        return self.string + ': ' + ' '.join(map(str, self.numbers))
 
-for d in range(7,8):
-    s = time.time()
-    
-    print(d, 'DIGITS')
-    lower, upper = 10**(d-1), 10**d - 1
-    primes = listPrimes(10**d)    
-    
-    occ = {}
-    
-    maxCount = 0
-    
-    for p in primes:
-        if p < lower:
-            continue
+num_digits = 6
+primes = listPrimes(10**num_digits)
+primes = [p for p in primes if len(str(p)) == num_digits]
+
+patterns = {}
+
+def num_to_pattern_string(n, to_replace):
+    pattern = list(str(n))
+    for i in range(len(pattern)):
+        if to_replace[i] == 1:
+            pattern[i] = 'X'
+    return ''.join(pattern)
+
+def compute_pattern(p, to_replace, digit):
+    if to_replace[0] == 1 and digit == 0:
+        return
+    if sum(to_replace) == 0:
+        return
+
+    valid = True
+    for i, c in enumerate(str(p)):
+        if to_replace[i] == 1 and int(c) != digit:
+            valid = False 
+            break
+    if valid: 
+        pattern_string = num_to_pattern_string(p, to_replace)
+        if pattern_string not in patterns:
+            patterns[pattern_string] = Pattern(pattern_string)
+
+        patterns[pattern_string].add(p)
         
-        string = str(p)
+
+# Replace all instances of 1 with the specified digit
+to_replace = list(product(range(2), repeat=num_digits))
+
+for p in primes:
+    for d in range(10): 
+        for r in to_replace:
+            compute_pattern(p, r, d)
+ 
+p, maxval = None, 0
+
+for k in patterns.keys():
+    if patterns[k].count > maxval:
+        maxval = patterns[k].count
+        p = k
         
-        # digit to replace (0,1,2,...)
-        for n in range(0,9):
-            
-            ind = findOccurences(string, str(n))
-            numIndices = len(ind)
-            
-            # number of instances of the digit to replace
-            for x in range(1,numIndices+1):
-                
-                # which digits to replace
-                for comb in combinations(ind, x):
-                    
-                    # generate the generic string
-                    generic = list(string)
-                    for i in comb:
-                        generic[i] = '*'
-                    generic = ''.join(generic)
-                    #print(p, generic)
-                    
-                    if generic not in occ:
-                        occ[generic] = 1
-                    else:
-                        occ[generic] += 1
-                        maxCount = max(maxCount, occ[generic])
-                        
-    print(maxCount)
-
-
-    print('time taken for {} digits: {:.2f} seconds'.format(d, time.time()-s))
+print(patterns[p].numbers)
+print(min(patterns[p].numbers), ',', patterns[p].count)
