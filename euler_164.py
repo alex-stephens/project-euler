01b538afec38e220e88e917a3bd78bc2dbe9706f
Alex Stephens <alexstephens9@gmail.com>
2022-11-03 14:16:17 +0000

    solved problem 164 - fun DP with lots of nested loops

--- euler_164.py
+++ euler_164.py
@@ -3,35 +3,34 @@
 
 # Consecutive digits sum
 
+import numpy as np
+from itertools import product
 
-maxsum = 9
-digits = 3
-totaldigits = 20
-# dp[n][x][d] is the number of ways to get the last d digits
-# up to n to sum to x
-
-dp = [[[0 for _ in range(digits+1)] for _ in range(maxsum+1)]
-            for _ in range(totaldigits+1)]
-
-# sum of previous 1 digit
-for n in range(totaldigits):
-    for val in range(0, maxsum-digits+2):
-        dp[n][val][1] = 1
-
-# first digit only
-for x in range(1,maxsum+1):
-    dp[1][x][1] = 1
-
-for d in range(2,digits+1):
-    for n in range(1,totaldigits+1):
-        for x in range(digits, maxsum+1):
-            for i in range(1,x-1):
-                dp[n][x][d] += dp[n-1][x-i][d-1]
-
-ans = sum([dp[20][x][2] for x in range(2,3)])
-print(dp)
+"""
+dp[n][d1][d2][d3] is the number of solutions
+with n digits, ending in d1, d2, d3.
+"""
 
 
-# given a KNOWN string of n digits where 3 consecutive never exceed 9:
+def main():
+    N = 20
+    limit = 9
+    
+    dp = np.zeros((N + 1, 10, 10, 10), dtype="int")
 
-# [x1 x2 x3 x4] 
+    # base case n = 3 with no leading zeros
+    for d1 in range(1, 10):
+        for d2, d3 in product(range(10), repeat=2):
+            if d1 + d2 + d3 <= limit:
+                dp[3][d1][d2][d3] = 1
+
+    for n in range(4, N + 1):
+        for d2, d3, x in product(range(10), repeat=3):
+            if d2 + d3 + x <= limit:
+                dp[n][d2][d3][x] += np.sum(dp, axis=1)[n - 1][d2][d3]
+
+    print(np.sum(dp, axis=(1, 2, 3))[N])
+
+
+if __name__ == "__main__":
+    main()
