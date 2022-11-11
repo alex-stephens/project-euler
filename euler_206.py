dcb400c30858ac934e142ea64d62f2628ca2f984
Alex Stephens <alexstephens9@gmail.com>
2022-11-11 16:51:51 +0000

    solved problem 206

--- euler_206.py
+++ euler_206.py
@@ -3,23 +3,37 @@
 
 # Concealed square
 
-import sys
-sys.path.append('..')
-from euler import isPerfectSquare
-from itertools import product
+from math import isqrt
 
-l = [str(x) for x in range(10)]
-num = '1234567890'
 
-def splice(str1, str2):
-    result = str1[0]
-    for i in range(len(str2)):
-        result += str2[i] + str1[i+1] 
-    return result
+def is_match(n):
+    target = "1_2_3_4_5_6_7_8_9_0"
+    s = str(n)
 
-for p in product(l,repeat=9):
-    string = splice(num, ''.join(p))
-    if isPerfectSquare(int(string)):
-        break
+    if len(s) != len(target):
+        return False
 
-print(int(int(string)**(1/2)))
+    for i, c in enumerate(target):
+        if c == "_":
+            continue
+        if c != s[i]:
+            return False
+    return True
+
+
+def main():
+    target_min = isqrt(1020304050607080900)
+    target_max = isqrt(1929394959697989990)
+
+    n = (target_min // 10) * 10
+
+    while n < target_max:
+        if is_match(n**2):
+            break
+        n += 10
+
+    print(n)
+
+
+if __name__ == "__main__":
+    main()
